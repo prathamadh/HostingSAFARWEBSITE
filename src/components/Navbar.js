0658c6cb535f5ccadc03b0c52d50440b3bab312b
pratham <apratham489@gmail.com>
2023-06-30 13:54:06 +0545

    fixed more bugs

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,43 +6,42 @@ import logo from '../image/logo.jpg';
 
 function Navbar() {
     return (
-        <section className="navbar mw-100">
-            <div className="logosection">
-                <ul id="logosection">
-                    <li>
-                        <img src={logo} alt="logo" className="image-responsive rounded-circle" style={{ height: "40px", width: "40px" }} />
+        <header class="header sticky top-0 bg-white shadow-md flex items-center justify-between px-8 py-02">
+          
+            <h1 class="w-3/12">
+                <a href="">
+                {/* <img className="h-10 w-10" src="C:\Users\acm\Desktop\ethoshacathon\SAFAR\src\components\collection\image\safarlogo.png" alt="" /> */}
+                <img src={logo} alt="logo" className="image-responsive rounded-circle" style={{ height: "50px", width: "50px" }} />
+                </a>
+            </h1>
+        
+            
+            <nav class="nav font-semibold text-lg">
+                <ul class="flex items-center">
+                    <li class="p-4 border-b-2 border-sky-500 border-opacity-0 hover:border-opacity-100 hover:text-sky-500 duration-200 cursor-pointer active">
+                      <a href="">Home</a>
                     </li>
-                    <li className="list font-bold font-mono ml-5 mt-2 text-zinc-700" id="logo"><span className="text-info  ">VIRTUAL</span>TOUR</li>
-
+                    <li class="p-4 border-b-2 border-sky-500 border-opacity-0 hover:border-opacity-100 hover:text-sky-500 duration-200 cursor-pointer">
+                      <a href="">360-View</a>
+                    </li>
+                    <li class="p-4 border-b-2 border-sky-500 border-opacity-0 hover:border-opacity-100 hover:text-sky-500 duration-200 cursor-pointer">
+                      <a href="">Explore</a>
+                    </li>
+                    <li class="p-4 border-b-2 border-sky-500 border-opacity-0 hover:border-opacity-100 hover:text-sky-500 duration-200 cursor-pointer">
+                      <a href="">About</a>
+                    </li>
                 </ul>
+            </nav>
+        
+           
+            <div class="w-3/12 flex justify-end">
+                <a href="" className="font-medium">Log In             </a>
+                
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md">
+                     Log in
+                </button>                
             </div>
-            <div className="list">
-                <ui id="lists">
-                    <li className="list" id="home">
-                    <a className="font-medium" href='/' >Home</a>
-                    </li>
-                    <li className="list" id="market">
-                    <a className="font-medium" href='#vrapp' >360-View</a>
-                    </li>
-                     <li className="list" id="Portfolio">
-                      <Link to='/explore' className="font-medium" >Explore</Link>
-                    </li>
-                    <li className="list" id="about">
-                    <a className="font-medium" href='#about' >About</a>
-                    </li>
-                   
-                </ui>
-            </div>
-            <div className="signup" >
-                <ul id="signup">
-                    <li className="font-medium">Log In</li>
-                    <li>
-                    <a font-medium href='#signup' >Get started free</a>
-                        </li>
-                </ul>
-            </div>
-
-        </section>
+        </header>
     );
 }
 export default Navbar;
