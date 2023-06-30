80dda6e384fd022faa0c09b0e23421bb820de774
pratham <apratham489@gmail.com>
2023-06-30 16:03:57 +0545

    added css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,16 +8,18 @@ function Navbar() {
     return (
         <header class="header sticky top-0 bg-white shadow-md flex items-center justify-between px-8 py-02">
           
-            <h1 class="w-3/12">
+            <h1 class="w-50 flex items-center  jusify-between">
                 <a href="">
                 {/* <img className="h-10 w-10" src="C:\Users\acm\Desktop\ethoshacathon\SAFAR\src\components\collection\image\safarlogo.png" alt="" /> */}
-                <img src={logo} alt="logo" className="image-responsive rounded-circle" style={{ height: "50px", width: "50px" }} />
-                </a>
+                <img src={logo} alt="logo" className="image-responsive rounded-circle" style={{ height: "60px", width: "60px" }} />
+                              </a>
+                              <h1 className="ml-4 font-bold text-3xl text-cyan-500">SAFAR {' '}{' '}{' '} {' '} {' '} {' '} {' '} {' '}   </h1>
             </h1>
-        
+          
             
             <nav class="nav font-semibold text-lg">
                 <ul class="flex items-center">
+                
                     <li class="p-4 border-b-2 border-sky-500 border-opacity-0 hover:border-opacity-100 hover:text-sky-500 duration-200 cursor-pointer active">
                       <a href="">Home</a>
                     </li>
@@ -35,9 +37,9 @@ function Navbar() {
         
            
             <div class="w-3/12 flex justify-end">
-                <a href="" className="font-medium">Log In             </a>
-                
-                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md">
+              
+                {/* <a font-medium href='#signup' >Get started free</a> */}
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 w-[70px] text-center rounded-md shadow-lg">
                      Log in
                 </button>                
             </div>
